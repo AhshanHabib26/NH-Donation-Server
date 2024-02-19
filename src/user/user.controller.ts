e72ace414efb0dbb55af351ee50e74c557065729
AhshanHabib26 <habib.net27@gmail.com>
2024-02-19 08:21:41 +0600

    Login API Added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { User } from "./user.model";
+import bcrypt from "bcrypt";
+const jwt = require("jsonwebtoken");
 
 const createUser = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
@@ -7,7 +9,7 @@ const createUser = async (req: Request, res: Response) => {
   const isUserExists = await User.findOne({ email });
 
   if (isUserExists) {
-    return res.status(401).json({
+    return res.status(400).json({
       sucess: false,
       message: "User Already Exists",
     });
@@ -17,11 +19,42 @@ const createUser = async (req: Request, res: Response) => {
 
   res.status(201).json({
     success: true,
-    message: "User Created Successfully",
+    message: "User created successfully",
     data: null,
+  });
+};
+
+const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid email or password",
+    });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid email or password",
+    });
+  }
+
+  const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "User login successful",
+    token,
   });
 };
 
 export const userController = {
   createUser,
+  loginUser,
 };
